33df86550cf7403ff8015c97d0df61fba94c6f0a
Vincent Liu <vliu1@cedar5.cedar.computecanada.ca>
2020-03-03 11:01:37 -0800

    remove unused files

--- scripts/torch_uoro_sweep.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-lr_list=(0.001)
-for lr in ${lr_list[@]}; do
-    python3 main_torch.py --dataset='cycleworld' --data_size 50000 --num_units=4 --model_name=uoro --state_update True --lr=$lr --num_run=10 --use_gpu=False --verbose=True
-done
